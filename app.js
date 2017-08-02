bf916ca7f32059daa84dba8b25927f9408f7687e
Natalia Hess <nathess91@gmail.com>
2017-08-01 22:10:11 -0700

    Creates next and previous image onclick events

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const apiKey = '72a030c9d43c47f1a4a31d87f636be6f';
 
-const giphyUrl = 'https://api.giphy.com/v1/gifs/search?q=sharks&api_key=72a030c9d43c47f1a4a31d87f636be6f';
+const giphyUrl = 'https://api.giphy.com/v1/gifs/search?q=puppies&api_key=72a030c9d43c47f1a4a31d87f636be6f';
 
-onSuccess = (json) => {
+showAllGifs = (json) => {
+  // default first picture inside featured div
   json.data.forEach((gif, i) => {
     const gallery = document.getElementById('gallery');
     // create parent column div
@@ -10,13 +11,38 @@ onSuccess = (json) => {
     col.setAttribute('class', 'col');
     // create image element
     const img = document.createElement('img');
-
     // append image to div
-    col.innerHTML = "<img src='" + gif.images.fixed_height.url + "' class='gallery-image' />";
-
+    col.innerHTML = "<img src='" + gif.images.fixed_height.url + "' class='gallery-image' id='" + i + "' />"
     // append div to gallery
     gallery.appendChild(col);
   });
+}
+
+let start = 0;
+
+nextImage = () => {
+  const images = document.getElementsByClassName('gallery-image');
+  let currentImage = images[start];
+  start++;
+
+  document.getElementById('featured').innerHTML = "<img src='" + currentImage.src + "' class='featured-image' id='" + currentImage.id + "' />";
+}
+
+previousImage = () => {
+  const images = document.getElementsByClassName('gallery-image');
+  let currentImage = images[start];
+  start--;
+
+  // if beginning of images, disable previous image functionality
+  if (currentImage === images[0]) {
+    document.getElementById('previous-button').style.visibility = 'hidden';
+  }
+
+  document.getElementById('featured').innerHTML = "<img src='" + currentImage.src + "' class='featured-image' id='" + currentImage.id + "' />";
+}
+
+onSuccess = (json) => {
+  showAllGifs(json);
 }
 
 onError = (status) => {
