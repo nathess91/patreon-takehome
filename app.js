16a2863f9159966858ab3e0f2ad3226529d6eb93
Natalia Hess <nathess91@gmail.com>
2017-08-02 11:51:20 -0700

    Adds ability to change featured image caption on click to selected image caption

--- app.js
+++ app.js
@@ -2,8 +2,24 @@ const apiKey = '&api_key=72a030c9d43c47f1a4a31d87f636be6f';
 
 const giphyUrl = 'https://api.giphy.com/v1/gifs/search?q=';
 
+// get caption
+// return no source
+// create element to contain caption
+
+getCaption = (username, source) => {
+  if (username) {
+    return username;
+  } else if (source) {
+    return source;
+  } else {
+    return 'No image source';
+  }
+}
+
 showAllGifs = (json) => {
   document.getElementById('featured').innerHTML = "<img src='" + json.data[0].images.fixed_height.url + "' class='featured-image' id='0' />"
+  document.getElementById('caption').innerHTML = '<p>' + getCaption(json.data[0].username, json.data[0].source_tld) + '</p>'
+
   json.data.forEach((gif, i) => {
     const gallery = document.getElementById('gallery');
     // create parent column div
@@ -12,7 +28,7 @@ showAllGifs = (json) => {
     // create image element
     const img = document.createElement('img');
     // append image to div
-    col.innerHTML = "<img src='" + gif.images.fixed_height.url + "' class='gallery-image' onclick='selectImage(this.src,this.id)' id='" + i + "' />"
+    col.innerHTML = "<img src='" + gif.images.fixed_height.url + "' class='gallery-image' onclick='selectImage(this.src,this.id)' id='" + i + "' /> <p id='" + i + "' style='display:none'>" + getCaption(gif.username, gif.source_tld) + "</p>";
     // append div to gallery
     gallery.appendChild(col);
   });
@@ -83,7 +99,15 @@ searchGifs = () => {
 }
 
 selectImage = (src, id) => {
+  // changes featured image source to selected image source
   document.getElementById('featured').innerHTML = "<img src='" + src + "' class='featured-image' id='" + id + "' />";
+  const pTags = document.getElementsByTagName('p');
+  // changes featured image caption to selected image caption
+  for (let tag in pTags) {
+    if (pTags[tag].id === id) {
+      document.getElementById('caption').innerHTML = '<p>' + pTags[tag].innerText + '</p>';
+    }
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
